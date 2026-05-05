bd3da2b59e8b8ffab30094719f1c1a94d2d277d1
Brendon W. <bwherry@midland.k12.ia.us>
2026-05-05 12:26:34 -0500

    Update exercise2.sql

--- exercise2.sql
+++ exercise2.sql
@@ -3,15 +3,15 @@ SET
     JOB_TITLE = (
         SELECT JOB_TITLE
         FROM Job_Info
-        WHERE Job_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
+        WHERE Job_Info.ID = Employee.ID
     ),
     SALARY = (
         SELECT SALARY
         FROM Job_Info
-        WHERE Job_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
+        WHERE Job_Info.ID = Employee.ID
     ),
     DEPARTMENT = (
         SELECT DEPARTMENT
         FROM Job_Info
-        WHERE Job_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
+        WHERE Job_Info.ID = Employee.ID
     );
