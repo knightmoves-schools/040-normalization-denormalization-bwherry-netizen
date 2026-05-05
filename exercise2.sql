490270350914c83f4dc4ea3aebdd6068cbbb30c5
Brendon W. <bwherry@midland.k12.ia.us>
2026-05-05 12:25:22 -0500

    Update exercise2.sql

--- exercise2.sql
+++ exercise2.sql
@@ -1,3 +1,17 @@
-INSERT INTO Employee (JOB_TITLE, SALARY, DEPARTMENT)
-SELECT JOB_TITLE, SALARY, DEPARTMENT
-FROM Job_Info;
+UPDATE Employee
+SET
+    JOB_TITLE = (
+        SELECT JOB_TITLE
+        FROM Job_Info
+        WHERE Job_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
+    ),
+    SALARY = (
+        SELECT SALARY
+        FROM Job_Info
+        WHERE Job_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
+    ),
+    DEPARTMENT = (
+        SELECT DEPARTMENT
+        FROM Job_Info
+        WHERE Job_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
+    );
