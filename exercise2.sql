0434ed3bf99195b448cb3a4f91967d18875e4845
Brendon W. <bwherry@midland.k12.ia.us>
2026-05-06 10:37:46 -0500

    Update exercise2.sql

--- exercise2.sql
+++ exercise2.sql
@@ -1,17 +1,1 @@
-UPDATE Employee
-SET 
-    JOB_TITLE = (
-        SELECT JOB_TITLE 
-        FROM Job_Info 
-        WHERE Job_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
-    ),
-    SALARY = (
-        SELECT SALARY 
-        FROM Job_Info 
-        WHERE Job_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
-    ),
-    DEPARTMENT = (
-        SELECT DEPARTMENT 
-        FROM Job_Info 
-        WHERE Job_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
-    );
+
