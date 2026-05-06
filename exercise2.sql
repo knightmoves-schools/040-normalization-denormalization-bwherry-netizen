c658dc5cc182330306320656e36485404344e423
Brendon W. <bwherry@midland.k12.ia.us>
2026-05-06 10:30:27 -0500

    Update exercise2.sql

--- exercise2.sql
+++ exercise2.sql
@@ -1,17 +1,1 @@
-UPDATE Employee
-SET
-    JOB_TITLE = (
-        SELECT JOB_TITLE
-        FROM Job_Info
-        WHERE Job_Info.ID = Employee.ID
-    ),
-    SALARY = (
-        SELECT SALARY
-        FROM Job_Info
-        WHERE Job_Info.ID = Employee.ID
-    ),
-    DEPARTMENT = (
-        SELECT DEPARTMENT
-        FROM Job_Info
-        WHERE Job_Info.ID = Employee.ID
-    );
+
