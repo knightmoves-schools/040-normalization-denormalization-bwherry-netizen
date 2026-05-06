01f7858036c4fdc6103604c7114406516bb427b8
Brendon W. <bwherry@midland.k12.ia.us>
2026-05-06 10:35:52 -0500

    Update exercise1.sql

--- exercise1.sql
+++ exercise1.sql
@@ -3,20 +3,20 @@ SET
     EMAIL = (
         SELECT EMAIL 
         FROM Contact_Info 
-        WHERE Contact_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
+        WHERE Contact_Info.EMPLOYEE_ID = Employee.ID
     ),
     PHONE_NUMBER = (
         SELECT PHONE_NUMBER 
         FROM Contact_Info 
-        WHERE Contact_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
+        WHERE Contact_Info.EMPLOYEE_ID = Employee.ID
     ),
     ADDRESS = (
         SELECT ADDRESS 
         FROM Contact_Info 
-        WHERE Contact_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
+        WHERE Contact_Info.EMPLOYEE_ID = Employee.ID
     ),
     ZIP_CODE = (
         SELECT ZIP_CODE 
         FROM Contact_Info 
-        WHERE Contact_Info.EMPLOYEE_ID = Employee.EMPLOYEE_ID
+        WHERE Contact_Info.EMPLOYEE_ID = Employee.ID
     );
